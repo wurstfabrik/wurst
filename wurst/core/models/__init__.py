75c8810046e3b54d591be14df9ad033eaa754f29
Aarni Koskela <akx@iki.fi>
2016-02-05 16:39:18 +0200

    Add Transition model

--- wurst/core/models/__init__.py
+++ wurst/core/models/__init__.py
@@ -1,3 +1,4 @@
 from .comments import Comment  # noqa
 from .issues import Issue, IssueType, Priority, Status  # noqa
 from .projects import Project  # noqa
+from .workflow import Transition  # noqa
